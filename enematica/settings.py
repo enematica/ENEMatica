b18cae6bb48657b84f2b1c9c888ba09ab2396911
Cristhian Michels <cristhianpostmichelscpm@gmail.com>
2026-08-06 12:58:36 -0300

    Adjust ALLOWED_HOSTS to deploy

--- enematica/settings.py
+++ enematica/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['enematica.vacaria.ifrs.edu.br', '*']
+ALLOWED_HOSTS = ['enematica.vacaria.ifrs.edu.br']
 
 
 # Application definition
